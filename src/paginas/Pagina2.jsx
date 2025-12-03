c2bf01dd66e79d0a67e6c390edb21f27a6f1a863
RedesBai <redes@bai-institute.com>
2025-12-03 13:00:13 +0100

    effect

--- src/paginas/Pagina2.jsx
+++ src/paginas/Pagina2.jsx
@@ -1,11 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './pagina2.css'
 
 function Pagina2() {
 
+  const [x, setX] = useState(0);
+
+  useEffect(() => {
+    console.log("Hola " + x);
+  }, [x]);
+  useEffect(() => {
+    const intervalo = setInterval(() => {
+      console.log("Intervalo " + x);
+    }, 1000);
+
+    return () => clearInterval(intervalo);
+  }, []);
+
   return (
     <>
       <h1>Hola Mundo desde Pagina 2</h1>
+      <button onClick={() => setX(x + 1)}>Incrementar</button>
     </>
   )
 }
